4651d39f4716c1f2fc2f3abeecf1b34d6dc439cb
Noah Laferty <noah.lafferty@gmail.com>
2015-06-09 13:11:51 -0500

    no flex zone

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $('nav span').mouseenter(function() {
 $('nav').mouseleave(function() {
 	$('nav').addClass('closed');
 })
+
+$('nav a').click(function() {
+	var page = $(this).parent().index();
+	console.log(page);
+	$('html,body').animate({
+      	scrollTop: $('.section-container > section').eq(page).offset().top
+    }, 500);
+})
